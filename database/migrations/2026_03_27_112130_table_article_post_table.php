234864396bc1ca7d1bce69f9ceb960ab469be1ce
ddd <ddd@fff>
2026-03-31 01:46:29 -0700

    complite Praktika_7

--- database/migrations/2026_03_27_112130_table_article_post_table.php
+++ database/migrations/2026_03_27_112130_table_article_post_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('user', function (Blueprint $table){
-            $table-dropColumn('email');
+            $table->dropColumn('email');
         });
     }
 };
--- database/migrations/2026_03_31_081928_age_equally_null.php
+++ database/migrations/2026_03_31_081928_age_equally_null.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -7,14 +8,15 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('user', function (Blueprint $table){
-            $table->text('email')->comment('user email adress')->change();
+        Schema::table("user", function (Blueprint $table) {
+        $table->integer("Age")->nullable();
         });
     }
+
     public function down(): void
     {
-        Schema::table('user', function (Blueprint $table){
-            $table->dropColumn('email');
+        Schema::table("user", function (Blueprint $table) {
+            $table->dropColumn("Age");
         });
     }
 };
