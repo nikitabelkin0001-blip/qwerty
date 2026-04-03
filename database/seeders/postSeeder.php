36d752ce2085da2fc323542c4784c94527dd61cb
ddd <ddd@fff>
2026-04-02 22:36:14 -0700

    Random generating

---
+++ database/seeders/postSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class postSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB:table('post')->insert([
+        [
+            "title"=>"Title",
+            "slug"=>"asd",
+            "likes"=>12,
+            "created_at"=>"12.12.12",
+            "updated_at"=>"27.02.12",
+        ],
+        [
+            "title"=>Str::random(10),
+            "slug"=>"65443",
+            "likes"=>44,
+            "created_at"=>"11.10.09",
+            "updated_at"=>"21.01.12",
+        ]
+        ]);
+    }
+}
