04cc4b9727ebd3930536d4583ff94c2eb996e569
ddd <ddd@fff>
2026-05-12 02:15:39 -0700

    Created model Post

---
+++ app/Http/Controllers/praktika10.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Fruitcake\LaravelDebugbar\Twig\Extension\Dump;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;    #Подключение фасада DB
+use PDO;
+
+class praktika10 extends Controller
+{
+    
+}
